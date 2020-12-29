f18dc74e3e995a225f59c2f8307c31f27b6122a7
Muna Ahmed <munaahmed@Munas-MacBook-Air.local>
2020-12-29 04:26:22 -0800

    final

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+      },
+    exercises:[{
+        type: {
+            type: String,
+            trim: true,
+            required: "Type of Exercise is Required"    
+        },
+
+         name: {
+            type: String,
+            trim: true,
+            required: "Name of Exercise is Required",
+        },
+
+        duration: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+           
+        },
+        reps: {
+            type: Number,
+           
+        },
+        sets: {
+            type: Number,
+           
+        },
+        
+    }]
+});
+
+let Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
